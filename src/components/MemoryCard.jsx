a17286f77779348859963604c76e17846faeeada
kpnguyen9 <kpnguyen2@gmail.com>
2022-03-31 20:38:57 -0500

    finished part 1

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MemoryCard extends React.Component {
+  render() {
+    return (
+      <>
+        <div class="MemoryCard">
+          <img src="./cardPic.png" alt="cardImage"></img>
+        </div>
+      </>
+    );
+  }
+}
+
+export default MemoryCard;
