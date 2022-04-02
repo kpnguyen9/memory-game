9153fedc7bdcfdfbe51b8067474ae235194a5336
kpnguyen9 <kpnguyen2@gmail.com>
2022-04-02 10:34:15 -0500

    completed part 2

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 class MemoryCard extends React.Component {
+  constructor() {
+    super();
+    this.state = { isFlipped: false };
+  }
+
+  clickHandler() {
+    this.setState({ isFlipped: !this.state.isFlipped });
+  }
+
   render() {
+    let memoryCardInnerClass = "MemoryCardInner";
+
+    if (this.state.isFlipped) {
+      memoryCardInnerClass += " flipped";
+    }
+
     return (
       <>
-        <div class="MemoryCard">
-          <img src="./cardPic.png" alt="cardImage"></img>
+        <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+          <div className={memoryCardInnerClass}>
+            <div className="MemoryCardBack">
+              <img src="./cardPic.png" alt="cardImage"></img>
+            </div>
+            <div className="MemoryCardFront">∆</div>
+          </div>
         </div>
       </>
     );
