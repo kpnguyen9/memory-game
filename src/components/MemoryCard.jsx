70f7cc617a74c99a0b3999b16e74614f6b02ec06
kpnguyen9 <kpnguyen2@gmail.com>
2022-04-02 13:01:39 -0500

    completed matching game

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,30 +1,21 @@
 import React from "react";
 
 class MemoryCard extends React.Component {
-  constructor() {
-    super();
-    this.state = { isFlipped: false };
-  }
-
-  clickHandler() {
-    this.setState({ isFlipped: !this.state.isFlipped });
-  }
-
   render() {
     let memoryCardInnerClass = "MemoryCardInner";
 
-    if (this.state.isFlipped) {
+    if (this.props.isFlipped) {
       memoryCardInnerClass += " flipped";
     }
 
     return (
       <>
-        <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+        <div className="MemoryCard" onClick={this.props.pickCard}>
           <div className={memoryCardInnerClass}>
             <div className="MemoryCardBack">
               <img src="./cardPic.png" alt="cardImage"></img>
             </div>
-            <div className="MemoryCardFront">∆</div>
+            <div className="MemoryCardFront">{this.props.symbol}</div>
           </div>
         </div>
       </>
